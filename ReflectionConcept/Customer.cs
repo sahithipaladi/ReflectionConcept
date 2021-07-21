c6ba519f087a52dc990dbd2840042fb86e48ead1
Sahithi Paladi <sahithi.p@tvsnext.io>
2021-07-21 07:24:58 -0700

    first commit

---
+++ ReflectionConcept/Customer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReflectionConcept
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int ID, string Name)
+        {
+            this.Id = ID;
+            this.Name = Name;
+        }
+
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+
+        public void PrintID()
+        {
+            Console.WriteLine("ID = {0}", this.Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name = {0}",this.Name);
+        }
+        
+
+        
+    }
+}
---
+++ ReflectionConcept/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionConcept
+{
+   
+        public class MainClass
+        {
+              private static void Main()
+              {
+                 Customer C1 = new Customer();
+                 C1.GetType();
+
+                 Type T = Type.GetType(" ReflectionConcept.Customer");
+                 Console.WriteLine("Full Name = {0}", T.FullName);
+                 Console.WriteLine("Just the Name= {0}", T.Name);
+                 Console.WriteLine("Just the Namespace = {0}", T.Namespace);
+                 Console.WriteLine();
+
+                 Console.WriteLine("Properties in Customer class");
+                 PropertyInfo[] properties = T.GetProperties();
+                 foreach (PropertyInfo property in properties)
+                 {
+                    Console.WriteLine(property.PropertyType.Name + " " + property.Name) ;
+                 }
+                 
+                 Console.WriteLine();
+                 Console.WriteLine("Methods in Customer class");
+                 MethodInfo[] methods = T.GetMethods();
+                 foreach (MethodInfo method in methods)
+                {
+                     Console.WriteLine(method.ReturnType.Name + " " + method.Name);
+                }
+
+                 Console.WriteLine();
+                 Console.WriteLine("Constructors in Customer class");
+                 ConstructorInfo[] constructors = T.GetConstructors();
+                 foreach (ConstructorInfo constructor in constructors)
+                 {
+                    Console.WriteLine(constructor.ToString());
+                 }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Fields in Customer class");
+            FieldInfo[] fields = T.GetFields();
+            foreach (FieldInfo field in fields)
+            {
+                Console.WriteLine(field.ReflectedType.Name + " " + field.Name);
+            
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
